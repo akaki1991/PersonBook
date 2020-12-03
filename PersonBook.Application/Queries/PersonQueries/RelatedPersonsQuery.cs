42afe1af671c6eed532514448906178f3cfe0da3
akaki1991 <akakichkopoia@gmail.com>
2020-12-03 16:40:29 +0400

    added PersonRealtionshipId and PersonRealtionshipType properties to RelatedPersonsQueryResultItem

--- PersonBook.Application/Queries/PersonQueries/RelatedPersonsQuery.cs
+++ PersonBook.Application/Queries/PersonQueries/RelatedPersonsQuery.cs
@@ -1,11 +1,11 @@
-﻿using FluentValidation;
-using FluentValidation.Attributes;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using PersonBook.Application.Infrastructure;
 using PersonBook.Application.Shared;
 using PersonBook.Domain.PersonAggregate;
 using PersonBook.Domain.PersonAggregate.ReadModels;
+using PersonBook.Domain.PersonRelationAggregate;
 using PersonBook.Domain.PersonRelationAggregate.ReaedModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -30,12 +30,12 @@ namespace PersonBook.Application.Queries.PersonQueries
             if (person == null)
                 return await FailAsync(ErrorCode.NotFound);
 
-            var firstAnsSecondPersonIds = await _db.Set<PersonRelationshipReadModel>()
+            var personRelationshipReadModel = await _db.Set<PersonRelationshipReadModel>()
                 .Where(x => x.FirstPersonId == Id || x.SecondPersonId == Id).ToListAsync();
 
-            var relationsIds = firstAnsSecondPersonIds.Select(x => x.FirstPersonId).Where(x => x != Id).ToList();
+            var relationsIds = personRelationshipReadModel.Select(x => x.FirstPersonId).Where(x => x != Id).ToList();
 
-            relationsIds.AddRange(firstAnsSecondPersonIds.Select(x => x.SecondPersonId).Where(x => x != Id).ToList());
+            relationsIds.AddRange(personRelationshipReadModel.Select(x => x.SecondPersonId).Where(x => x != Id).ToList());
 
             var releatedPersons = await _db.Set<PersonReadModel>()
                 .Where(x => relationsIds.Contains(x.Id))
@@ -44,7 +44,7 @@ namespace PersonBook.Application.Queries.PersonQueries
                 .Take(PageSize)
                 .ToListAsync();
 
-            var result = releatedPersons.Select(x => new PersonDetailsQueryResult
+            var result = releatedPersons.Select(x => new RelatedPersonsQueryResultItem
             {
                 Id = x.AggregateRootId,
                 FirstName = x.FirstName,
@@ -80,7 +80,13 @@ namespace PersonBook.Application.Queries.PersonQueries
                         Type = PhoneNumberType.Office,
                         Number = person.OfficePhoneNumber
                     }
-                }
+                },
+                PersonRealtionshipId = personRelationshipReadModel
+                        .FirstOrDefault(r => r.FirstPersonId == x.AggregateRootId
+                                        || r.SecondPersonId == x.AggregateRootId).Id,
+                PersonRelationshipType = personRelationshipReadModel
+                        .FirstOrDefault(r => r.FirstPersonId == x.AggregateRootId
+                                        || r.SecondPersonId == x.AggregateRootId).PersonRelationshipType
             }).ToList();
 
             return await OkAsync(new RelatedPersonsQueryResult
@@ -92,6 +98,32 @@ namespace PersonBook.Application.Queries.PersonQueries
 
     public class RelatedPersonsQueryResult
     {
-        public IEnumerable<PersonDetailsQueryResult> RelatedPersons { get; set; }
+        public IEnumerable<RelatedPersonsQueryResultItem> RelatedPersons { get; set; }
     }
+
+    public class RelatedPersonsQueryResultItem
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public string PersonalNumber { get; set; }
+
+        public DateTimeOffset BirthDate { get; set; }
+
+        public City City { get; set; }
+
+        public IEnumerable<PhoneNumber> PhoneNumbers { get; set; }
+
+        public Photo Photo { get; set; }
+
+        public int PersonRealtionshipId { get; set; }
+
+        public PersonRelationshipType PersonRelationshipType { get; set; }
+    }
+
 }
